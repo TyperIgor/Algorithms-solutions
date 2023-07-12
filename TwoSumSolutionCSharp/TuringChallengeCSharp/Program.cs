5715a31e8290f25810e6cd7e678d79fa0f35841a
TyperIgor <igor_purosso@hotmail.com>
2023-07-11 23:19:08 -0300

    update turing solution

---
+++ TwoSumSolutionCSharp/TuringChallengeCSharp/Program.cs
@@ -0,0 +1,28 @@
+﻿
+
+// Solution using HashSet to find duplicates numbers with complexity O(n) --
+
+// Should Find numbers that repeat itself and return the number that fit position
+
+// Example 1:
+// input --> { 1, 2, 3, 2 };
+
+// output [2,4]
+
+//Example 2:  
+//input {1,2,3,4,3} | output [3,5]
+
+static int[] FindErrorNums(int[] nums)
+{
+    HashSet<int> numsReplited = new();
+
+    for (int i = 0; i <= nums.Length - 1; i++)
+    {
+        if (!numsReplited.Add(nums[i]))
+            return new int[] { nums[i], i += 1 };
+
+        numsReplited.Add(nums[i]);
+    }
+
+    return new int[0];
+}
